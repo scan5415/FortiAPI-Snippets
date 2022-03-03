5baae120f3f6504ada071308a4d7d480c5abbe09
Andy Scherer <nomail@scherer.me>
2022-03-03 10:11:31 +0100

    Create File

---
+++ Get-SwitchPorts.py
@@ -0,0 +1,40 @@
+import requests
+import json
+import urllib3
+import csv
+import os
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+base_url = "https://62.2.122.4:10000/api/v2/"
+url_login= base_url + "authentication"
+
+csv_header = ['port','untagged','tagged']
+csv_path = os.path.dirname(__file__)
+# Start Session to keep cookies
+sess = requests.session()
+
+#Login request
+payload = {'username' : 'fwadmin', 'secretkey' : 'R1kQXAGsxHCL'}
+res = sess.post(url_login, json=payload, verify=False )
+
+# Read CSRFTOKEN and add it to the Header
+for cookie in sess.cookies:
+            if 'ccsrftoken' in cookie.name:
+                csrftoken = cookie.value[1:-1]
+                sess.headers.update({'X-CSRFTOKEN': csrftoken})
+
+res = sess.get(base_url + "/cmdb/switch-controller/managed-switch", verify=False)
+
+resp_dict = res.json()
+for switch in resp_dict['results']:
+    ports = []
+
+    print("Switch: %s" % (switch['name']))
+
+    with open(csv_path + switch['name'] + ".csv", 'w',newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(csv_header)
+
+        for port in switch['ports']:
+            port = [port['port-name'], port['vlan'], port['allowed-vlans']]
+            writer.writerow(port)
